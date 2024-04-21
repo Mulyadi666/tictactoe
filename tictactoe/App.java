bd479873d1d27627fe35aca84d7e9499d06e4655
Mulyadi666 <107296052+Mulyadi666@users.noreply.github.com>
2024-04-21 13:00:21 +0700

    Add files via upload

---
+++ tictactoe/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tictactoe;
+
+/**
+ *
+ * @author adimu
+ */
+public class App {
+    public static void main(String[] args) throws Exception{
+        Tictactoe tictactoe = new Tictactoe();
+    }
+    
+}
